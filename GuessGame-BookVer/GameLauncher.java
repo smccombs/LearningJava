fa41367e402e91aaf30b68208880313c0fb4504c
Shawn McCombs <shawnmccombsit@gmail.com>
2015-10-25 17:19:42 -0500

    Updated j.bat to create bin dir per program

--- GuessGame-BookVer/GameLauncher.java
+++ GuessGame-BookVer/GameLauncher.java
@@ -3,5 +3,4 @@ public class GameLauncher {
 		GuessGame game = new GuessGame();
 		game.startGame();
 	}
-
 }
--- j.bat
+++ j.bat
@@ -3,5 +3,9 @@ SET JavaPath=%1
 SET JavaExec=%2
 cd %JavaPath%
 javac *.java
+IF NOT EXIST bin mkdir bin
+move *.class bin >nul
+cd bin
 java %JavaExec%
+cd ..
 cd ..
